789301d3212468fd3281ca8b2dd60066a8be00e6
Nicolas Ariel Anzevino <NicolasAnzevino@hotmail.com>
2023-09-22 15:36:25 -0300

    Gestion de Solicitudes y Clases: Primer Avance

---
+++ Data/Solicitud_SoporteRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProyectoTFI.Service;
+using ProyectoTFI.Entities;
+
+namespace ProyectoTFI.Data
+{
+    public class Solicitud_SoporteRepository
+    {
+        TFIContext context;
+        public Solicitud_SoporteRepository()
+        {
+            context = new TFIContext();
+        }
+
+        public List<Solicitud_Soporte> ListarSolicitudes(string pBusqueda, string pEstado, int pAlumnoID)
+        {
+            List<Solicitud_Soporte> LSS = new List<Solicitud_Soporte>();
+            if (!String.IsNullOrEmpty(pBusqueda))
+            {
+                if (pEstado == "Activo" || pEstado is null)
+                {
+                    LSS = context.Solicitud_Soporte.Where(x => ((x.AlumnoID == pAlumnoID) && (x.Activo == true)) && ((x.ID.ToString().Contains(pBusqueda)) || (x.Descripcion.Contains(pBusqueda)))).ToList();     
+                }
+                else
+                {
+                    LSS = context.Solicitud_Soporte.Where(x => ((x.AlumnoID == pAlumnoID) && (x.Activo == false)) && ((x.ID.ToString().Contains(pBusqueda)) || (x.Descripcion.Contains(pBusqueda)))).ToList();
+                }
+            }
+            else
+            {
+                if (pEstado == "Activo" || pEstado is null)
+                {
+                    LSS = context.Solicitud_Soporte.Where(x => (x.AlumnoID == pAlumnoID) && (x.Activo == true)).ToList();
+                }
+                else
+                {
+                    LSS = context.Solicitud_Soporte.Where(x => (x.AlumnoID == pAlumnoID) && (x.Activo == false)).ToList();
+                }
+            }
+            return LSS;
+        }
+
+        public bool AgregarSolicitudSoporte(Solicitud_Soporte pSolicitud_Soporte)
+        {
+            context.Solicitud_Soporte.Add(pSolicitud_Soporte);
+
+            return context.SaveChanges() > 0;
+        }
+
+        public Solicitud_Soporte VerSolicitud(int id)
+        {
+            Solicitud_Soporte Solicitud = context.Solicitud_Soporte.Where(u => u.ID == id).FirstOrDefault();
+
+            return Solicitud;
+        }
+    }
+}
---
+++ Models/Solicitud_SoporteViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using ProyectoTFI.Entities;
+
+namespace ProyectoTFI.Models
+{
+    public class Solicitud_SoporteViewModel
+    {
+        public Solicitud_SoporteViewModel()
+        {
+            this.Solicitud_Respuesta = new HashSet<Solicitud_Respuesta>();
+        }
+
+        public Solicitud_SoporteViewModel(Solicitud_Soporte pSoliSoporte)
+        {
+            ID = pSoliSoporte.ID;
+            Asunto = pSoliSoporte.Asunto;
+            TipoConsulta = pSoliSoporte.TipoConsulta;
+            Descripcion = pSoliSoporte.Descripcion;
+            Fecha = pSoliSoporte.Fecha;
+            Activo = pSoliSoporte.Activo;
+            AlumnoID = pSoliSoporte.AlumnoID;
+
+            this.Solicitud_Respuesta = new HashSet<Solicitud_Respuesta>();
+        }
+
+
+        public int ID { get; set; }
+        public string Asunto { get; set; }
+        public string TipoConsulta { get; set; }
+        public string Descripcion { get; set; }
+        public System.DateTime Fecha { get; set; }
+        public bool Activo { get; set; }
+        public int AlumnoID { get; set; }
+        public virtual ICollection<Solicitud_Respuesta> Solicitud_Respuesta { get; set; }
+
+    }
+}
