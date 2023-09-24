8abc6b6d2722eab0b46254bac02bbb22f7d172fb
facusicart <facusicart@gmail.com>
2023-09-24 17:40:30 -0300

    Arreglos Cursos

--- Data/CursoRepository.cs
+++ Data/CursoRepository.cs
@@ -15,58 +15,30 @@ namespace ProyectoTFI.Data
             context = new TFIContext();
         }
 
-        public List<Curso> ListarCursos(string pBusqueda, string pActivo)
-        {
-            List<Curso> LU = new List<Curso>();
-            if (!String.IsNullOrEmpty(pBusqueda))
-            {
-                if (pActivo == "Activo" || pActivo is null)
-                {
-                    LU = context.Curso.Where((c => (c.Activo == true) && (c.Nombre.Contains(pBusqueda)))).ToList();
-                }
-                else
-                {
-                    LU = context.Curso.Where((c => (c.Activo == false) && (c.Nombre.Contains(pBusqueda)))).ToList();
-                }
-            }
-            else
-            {
-                if (pActivo == "Activo" || pActivo is null)
-                {
-                    LU = context.Curso.Where(c => c.Activo == true).ToList();
-                }
-                else
-                {
-                    LU = context.Curso.Where(c => c.Activo == false).ToList();
-                }
-            }
-            return LU;
-            //return context.Usuario.Where(u => u.RolID == 2 && (u.Nombre.Contains(pBusqueda) || u.Apellido.Contains(pBusqueda) || u.Username.Contains(pBusqueda)) || pBusqueda == null).ToList();
-        }
-        public List<Curso> ListarCursosUsuario(int id, string pBusqueda, string pActivo)
+        public List<Curso> ListarCursos(string pBusqueda)
         {
             List<Curso> LC = new List<Curso>();
             if (!String.IsNullOrEmpty(pBusqueda))
             {
-                if (pActivo == "Activo" || pActivo is null)
-                {
-                    LC = context.Curso.Include("Cursada_de_Alumno").Where((c => (c.Activo == true && id == c.ID) && (c.Nombre.Contains(pBusqueda)))).ToList();
-                }
-                else
-                {
-                    LC = context.Curso.Where((c => (c.Activo == false) && (c.Nombre.Contains(pBusqueda)))).ToList();
-                }
+                LC = context.Curso.Where((c => (c.Activo == true) && (c.Nombre.Contains(pBusqueda)))).ToList();
             }
             else
             {
-                if (pActivo == "Activo" || pActivo is null)
-                {
-                    LC = context.Curso.Where(c => c.Activo == true).ToList();
-                }
-                else
-                {
-                    LC = context.Curso.Where(c => c.Activo == false).ToList();
-                }
+                LC = context.Curso.Where(c => c.Activo == true).ToList();
+            }
+            return LC;
+            //return context.Usuario.Where(u => u.RolID == 2 && (u.Nombre.Contains(pBusqueda) || u.Apellido.Contains(pBusqueda) || u.Username.Contains(pBusqueda)) || pBusqueda == null).ToList();
+        }
+        public List<Curso> ListarCursosUsuario(int id, string pBusqueda)
+        {
+            List<Curso> LC = new List<Curso>();
+            if (!String.IsNullOrEmpty(pBusqueda))
+            {
+                LC = context.Curso.Where((c => (c.Activo == true) && (c.Nombre.Contains(pBusqueda)))).ToList();
+            }
+            else
+            {
+                 LC = context.Curso.Where(c => c.Activo == true).ToList();
             }
             return LC;
             //return context.Usuario.Where(u => u.RolID == 2 && (u.Nombre.Contains(pBusqueda) || u.Apellido.Contains(pBusqueda) || u.Username.Contains(pBusqueda)) || pBusqueda == null).ToList();
